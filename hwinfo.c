bbae21e5b4b066370972deb3cf8bc2d49b3b6d45
Michael <unixandtrees@gmail.com>
2014-07-30 01:17:23 -0500

    added conrol_status register

--- hwinfo.c
+++ hwinfo.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <sys/mman.h>
 #include <inttypes.h>
 #define CONTROL   0x44E10000
+#define CONTROL_STATUS    0x40
 #define DEVICE_ID 0x600
 #define END       0x44E11444
 
 int main() {
-  volatile uint32_t *dev_info;
+  volatile uint32_t *dev_info, *status;
   int fd = open("/dev/mem", O_RDWR);
 
   if(fd == -1) {
@@ -22,8 +24,10 @@ int main() {
     exit(1);
   }
 
+  status = base + (CONTROL_STATUS);
   dev_info = base + (DEVICE_ID);
-  printf("%x\n", *(volatile uint32_t *)dev_info);
+  printf("Hardware Info: %x\n", *(volatile uint32_t *)status);
+  printf("Device Info: %x\n", *(volatile uint32_t *)dev_info);
 
   close(fd);
   return 0;
