cef014ab70348a36ed1a004f54a3f04b7661fb22
Michael <unixandtrees@gmail.com>
2014-07-30 00:50:34 -0500

    initial commit

---
+++ hwinfo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <inttypes.h>
+#define CONTROL   0x44E10000
+#define DEVICE_ID 0x600
+#define END       0x44E11444
+int main() {
+  int fd = open("/dev/mem", O_RDWR);
+  if(fd == -1) {
+    printf("COULDN'T OPEN FILE\n");
+    exit(1);
+  }
+  volatile uint32_t *dev_info;
+  volatile void *base = mmap(0, END-CONTROL, PROT_READ | PROT_WRITE,
+                                MAP_SHARED, fd, CONTROL); 
+  if(mmap == MAP_FAILED) {
+    printf("MAP FAILED\n");
+    exit(1);
+  }
+  dev_info = base + (DEVICE_ID);
+  printf("%x\n", *(volatile uint32_t *)dev_info);
+}
