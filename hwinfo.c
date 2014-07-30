b17754c6d0d7a35e985db2c9c5234faeef4d3cf3
Michael <unixandtrees@gmail.com>
2014-07-30 00:54:56 -0500

    closed file

--- hwinfo.c
+++ hwinfo.c
@@ -6,19 +6,25 @@
 #define CONTROL   0x44E10000
 #define DEVICE_ID 0x600
 #define END       0x44E11444
+
 int main() {
+  volatile uint32_t *dev_info;
   int fd = open("/dev/mem", O_RDWR);
+
   if(fd == -1) {
     printf("COULDN'T OPEN FILE\n");
     exit(1);
   }
-  volatile uint32_t *dev_info;
   volatile void *base = mmap(0, END-CONTROL, PROT_READ | PROT_WRITE,
                                 MAP_SHARED, fd, CONTROL); 
   if(mmap == MAP_FAILED) {
     printf("MAP FAILED\n");
     exit(1);
   }
+
   dev_info = base + (DEVICE_ID);
   printf("%x\n", *(volatile uint32_t *)dev_info);
+
+  close(fd);
+  return 0;
 }
